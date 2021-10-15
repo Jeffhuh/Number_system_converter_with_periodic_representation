d82e51dcb4b314bd54a11390a61dbcc4ba782467
Jeffhuh <41673759+Jeffhuh@users.noreply.github.com>
2021-10-15 16:23:27 +0200

    Update Main.java

--- v3/Main.java
+++ v3/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//chwatal@spengergasse.at
 		//System.out.println("convertToDecimal number numbersystem lengthBeforePeriod lengthOfPeriod");
 		System.out.println("[number] [numbersystem of the input number] [numbersystem of the output number] [lengthOfPeriod]");
 		Scanner scan = new Scanner(System.in);
